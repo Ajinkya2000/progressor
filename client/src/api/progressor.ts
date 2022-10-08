6fc58495fb4f115d90dccf609b2a5e31ca7d622f
Ajinkya Deshpande <ajinkya.deshpande0000@gmail.com>
2022-10-08 12:11:29 +0530

    update api url to https

--- client/src/api/progressor.ts
+++ client/src/api/progressor.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 let URL;
 
 if (process.env.NODE_ENV === "production") {
-	URL = "http://progressor-web.herokuapp.com/api/";
+	URL = "https://progressor-web.herokuapp.com/api/";
 } else {
 	URL = "http://localhost:8000/api/";
 }
